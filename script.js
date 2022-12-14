f4918b2f316a0d35613a991595eb75c30f0127a8
leocaridade <leocaridade@hotmail.com>
2022-12-14 15:29:12 -0500

    Resolve o requisito 2

--- script.js
+++ script.js
@@ -7,6 +7,43 @@ const createTitle = () => {
   body.appendChild(title);
 };
 
+const createSection = () => {
+  const section = document.createElement('section');
+  section.id = 'color-palette';
+  section.style.display = 'flex';
+  body.appendChild(section);
+};
+
+const createDiv = () => {
+  for (let index = 0; index < 4; index += 1) {
+    const section = document.getElementById('color-palette');
+    const div = document.createElement('div');
+    div.className = 'color';
+    div.style.border = 'solid 1px black';
+    div.style.width = '20px';
+    div.style.height = '20px';
+    section.appendChild(div);
+  }
+};
+
+const colorDiv = () => {
+  const div = document.getElementsByClassName('color');
+  for (let index = 0; index < div.length; index += 1) {
+    if (index === 0) {
+      div[index].style.backgroundColor = 'black';
+    } else if (index === 1) {
+      div[index].style.backgroundColor = 'green';
+    } else if (index === 2) {
+      div[index].style.backgroundColor = 'red';
+    } else if (index === 3) {
+      div[index].style.backgroundColor = 'blue';
+    }
+  }
+};
+
 window.onload = () => {
   createTitle();
+  createSection();
+  createDiv();
+  colorDiv();
 };
